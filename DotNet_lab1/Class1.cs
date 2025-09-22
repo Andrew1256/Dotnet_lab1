9157f86248cb72bf10a57e82f8a310656652cf4c
Andrew <andrijvasilik252@gmail.com>
2025-09-22 17:55:14 +0300

    finish task

--- DotNet_lab1/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DotNet_lab1
-{
-    public class Class1
-    {
-        const int MyConst = 42;
-    }
-}
---
+++ JonDou9000.TaskPlanner.Domain.Logic/JonDou9000.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,18 @@
+﻿using JonDou9000.TaskPlanner.Domain.Models;
+using System;
+using System.Linq;
+
+namespace JonDou9000.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        public WorkItem[] CreatePlan(WorkItem[] items)
+        {
+            return items
+                .OrderByDescending(x => x.Priority)   // 1. Пріоритет (High > Medium > Low)
+                .ThenBy(x => x.DueDate)               // 2. Дата дедлайну (раніше — перше)
+                .ThenBy(x => x.Title)                 // 3. Алфавітний порядок
+                .ToArray();
+        }
+    }
+}
